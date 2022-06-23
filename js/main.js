2318fda8260e2b44760a9c4585e1519791356588
albertallan <79917446+albertallan-rar@users.noreply.github.com>
2022-06-23 20:50:37 -0300

    Estrutura Front-End
    Estrutura front-end inicial finalizada e tem como principio o uso no mobile porem a estrutura pode ser visualizada em um desktop.
    
    Portabilidade para desktop sendo desenvolvida no momento

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+window.addEventListener('scroll', rolagem)
+
 function rolagem() {
+  showNavOnScroll()
+  showBackToTopButtonScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY == 0) {
     navegador.classList.remove('scroll')
   } else {
     navegador.classList.add('scroll')
+  }
+}
+
+function showBackToTopButtonScroll() {
+  if (scrollY < 500) {
+    BackToTopButton.classList.remove('show')
+  } else {
+    BackToTopButton.classList.add('show')
   }
 }
 
@@ -13,3 +28,18 @@ function menuexpansion() {
 function exitmenuexpansion() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1500
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about h4,
+  #about h2,
+  #about p`)
