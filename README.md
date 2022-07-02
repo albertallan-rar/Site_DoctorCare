9181ae9420a234b06fb20d75d3fd5eadeb5f9643
albertallan <79917446+albertallan-rar@users.noreply.github.com>
2022-07-01 21:46:35 -0300

    Versão final do Projeto :tada:
    Projeto front-end finalizado com sucesso, versão mobile e desktop funcionando perfeitamente :tada: :tada:

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Site DoctorCare
+
 Site com as finalidades estudantis para aprimorar as praticas em HTML, CSS e JS
+
+Foi utilizando na criação do site o tutorial disponibilizado pela Rocketseat no NLW Return
+
+Link do site para você conferir o resultado:
+https://albertallan-rar.github.io/Site_DoctorCare/
+
--- js/main.js
+++ js/main.js
@@ -3,6 +3,68 @@ window.addEventListener('scroll', rolagem)
 function rolagem() {
   showNavOnScroll()
   showBackToTopButtonScroll()
+  activateMenuAtCurrenctSector(home)
+  activateMenuAtCurrenctSector(services)
+  activateMenuAtCurrenctSector(about)
+  activateMenuAtCurrenctSector(contact)
+}
+
+function activateMenuAtCurrenctSector(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+
+  //console.log(services.offsetTop) Mostra qual o numero que é indentificado o topo da tela
+  //console.log(services.offsetHeight) Mostra o numero que é indentificado o final
+  // offset significa deslocamento
+
+  //SEMPRE QUE FOR CODIFICAR, PENSAR EM TODOS OS DADOS NECESSARIOS PARA
+  //REALIZAR A AÇÃO
+
+  //QUAIS DADOS VOU PRECISAR ???
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+
+  //a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  //O topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  //informações dos dados e da logica
+  // console.log(
+  //   'O topo da sessão chegou ou passou da linha ?',
+  //   sectionTopReachOrPassedTargetline
+  // )
+
+  //verificar se a base está abaixo da linha alvo
+
+  //QUAIS DADOS VOU PRECISAR ???
+
+  //a sessão termina onde
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da sessão passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  // console.log(
+  //   'O fundo da sessão chegou ou passou da linha ?',
+  //   sectionEndPassedTargetline
+  // )
+
+  //limite da sessãp
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}] `)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
